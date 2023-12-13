98ef567a1235de4969ac8209ef9532f059353344
JunsoMin <minjs4562@gmail.com>
2023-12-13 21:23:55 +0900

    lec16 initial commit

--- scripts/review/Lec15/src/ConstructorRefDEmo.java
+++ scripts/review/Lec15/src/ConstructorRefDEmo.java
@@ -16,7 +16,7 @@ class MyClass00{
 public class ConstructorRefDEmo {
     public static void main(String[] args) {
 
-        Refinterface classCons = MyClass00::new;    // 생성자 참조
+        Refinterface classCons = MyClass00::new;    // 생성자 참조   제네릭 생성자의 경우 Myclass<T>::new 이런식으로 해야
         MyClass00 mc = classCons.refunc(10);    // 객체 생성
 
         System.out.println(mc.getVal());
